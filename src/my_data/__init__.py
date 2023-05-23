2697a09def272c513d358f1f61bc6570f0c19d60
Daryl Stark <daryl@dstark.nl>
2023-05-23 16:28:50 +0200

    Added a docstring to the package

--- src/my_data/__init__.py
+++ src/my_data/__init__.py
@@ -1,6 +1,7 @@
 """My Data Package.
 
-This package contains the code 
+This package contains the code to create, retrieve, update and elete data from
+a database containing the My Model.
 """
 
 __version__ = '0.0.1-dev'
