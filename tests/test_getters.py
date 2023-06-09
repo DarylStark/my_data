c0ec43042fa7c9420b56299cb53407dcdd94dce4
Daryl Stark <daryl@dstark.nl>
2023-06-09 11:14:05 +0200

    Restructured the testing

--- tests/test_getters.py
+++ tests/test_getters.py
@@ -1,82 +1,11 @@
-from pytest import fixture
+from fixtures_db_creation import *
+from my_model.user import User
 
-from my_data.db_connection import db_connection
-from my_data.context import Context
 from database.database import Database
-from database.factories import create_memory_sqlite_database
-from my_data.db_models import DBTag, DBUser
-from my_model.user import User, UserRole
-from datetime import datetime
+from my_data.context import Context
 
 
-@fixture
-def root_user() -> User:
-    return User(
-        id=1,
-        fullname='Root',
-        username='root',
-        email='root@dstark.nl',
-        role=UserRole.ROOT,
-        password_hash='asdasdas',
-        password_date=datetime.now())
-
-
-@fixture
-def normal_user() -> User:
-    return User(
-        id=2,
-        fullname='Daryl Stark',
-        username='daryl.stark',
-        email='daryl@dstark.nl',
-        role=UserRole.USER,
-        password_hash='asdasdas',
-        password_date=datetime.now())
-
-
-@fixture
-def db() -> Database:
-    # Create a connection to the in-memory database
-    create_memory_sqlite_database(db_connection)
-
-    # Create the tables
-    db_connection.create_tables(drop_tables=False)
-
-    with db_connection.get_session() as a:
-        # Root user
-        root = DBUser(
-            fullname='Root',
-            username='root',
-            email='root@dstark.nl',
-            role=UserRole.ROOT,
-            password_hash='asdasdas',
-            password_date=datetime.now())
-        root.tags = [
-            DBTag(title='test_root_1'),
-            DBTag(title='test_root_2'),
-        ]
-        a.add(root)
-
-        # User for Daryl Stark
-        daryl = DBUser(
-            fullname='Daryl Stark',
-            username='daryl.stark',
-            email='daryl@dstark.nl',
-            role=UserRole.USER,
-            password_hash='asdasdas',
-            password_date=datetime.now())
-        daryl.tags = [
-            DBTag(title='test_daryl_1'),
-            DBTag(title='test_daryl_2')
-        ]
-        a.add(daryl)
-
-        a.commit()
-
-    # Return the connection object
-    return db_connection
-
-
-def test_tags(db: Database, normal_user: User) -> None:
+def test_tags(db: Database, normal_user: User, pytestconfig) -> None:
     with Context(user=normal_user) as c:
         tags = c.tags.get()
         assert tags[0].title == 'test_daryl_1'
