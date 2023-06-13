c44635ab038b7e2900de9a42220f6c566da368fe
Daryl Stark <daryl@dstark.nl>
2023-06-13 20:22:06 +0200

    Added some extra tests

--- tests/test_getters.py
+++ tests/test_getters.py
@@ -3,11 +3,11 @@
 Module that contains tests for the getters of the ResourceManager.
 """
 from my_model.user import User
+from pytest import raises
 
 from database.database import Database
 from my_data.context import Context
-
-from my_data.db_models import DBTag, Tag
+from my_data.db_models import DBTag, DBUser
 
 
 def test_get_users_normal(db: Database, normal_user: User) -> None:
@@ -63,10 +63,70 @@ def test_tags(db: Database, normal_user: User) -> None:
         assert tags[1].title == 'test_daryl_2'
 
 
-def test_tags_filters(db: Database, normal_user: User) -> None:
-    """Test to retrieve tags for a specific user with a filter.
+def test_users_raw_filters(db: Database, root_user: User) -> None:
+    """Test to retrieve users as root with a raw filter.
 
-    Should retrieve tags for the user in the context based on specific filters.
+    Should retrieve users the context based on specific raw filters.
+
+    Args:
+        db: the database connection. Not used right now, but still in there to
+            make sure the database is connected.
+        root_user: the model for the root user.
+    """
+    with Context(user=root_user) as c:
+        # Test specific match
+        users = c.users.get(raw_filters=[DBUser.username == 'root'])
+        assert users[0].username == 'root'
+        assert len(users) == 1
+
+        # Test specific unmatch
+        users = c.users.get(raw_filters=[DBUser.username != 'root'])
+        assert users[0].username == 'daryl.stark'
+        assert len(users) == 1
+
+        # Test bigger then
+        users = c.users.get(raw_filters=[DBUser.id > 0])
+        assert users[0].username == 'root'
+        assert users[1].username == 'daryl.stark'
+        assert len(users) == 2
+
+        # Test smaller then
+        users = c.users.get(raw_filters=[DBUser.id < 99])
+        assert users[0].username == 'root'
+        assert users[1].username == 'daryl.stark'
+        assert len(users) == 2
+
+
+def test_users_named_filters(db: Database, root_user: User) -> None:
+    """Test to retrieve tags for a specific user with named filters.
+
+    Should retrieve users for the the context based on specific named filters.
+
+    Args:
+        db: the database connection. Not used right now, but still in there to
+            make sure the database is connected.
+        root_user: the model for the root user.
+    """
+    with Context(user=root_user) as c:
+        # Test specific match
+        users = c.users.get(username='daryl.stark')
+        assert users[0].username == 'daryl.stark'
+        assert len(users) == 1
+
+        # Test if something that is invalid is not found indeed
+        users = c.users.get(username='emilia.clarke')
+        assert len(users) == 0
+
+        # Test if a invalid field results in a exception
+        with raises(AttributeError):
+            users = c.users.get(unknown_field='test')
+
+
+def test_tags_raw_filters(db: Database, normal_user: User) -> None:
+    """Test to retrieve tags for a specific user with a raw filter.
+
+    Should retrieve tags for the user in the context based on specific raw
+    filters.
 
     Args:
         db: the database connection. Not used right now, but still in there to
@@ -74,5 +134,50 @@ def test_tags_filters(db: Database, normal_user: User) -> None:
         normal_user: the model for the normal user.
     """
     with Context(user=normal_user) as c:
-        tags = c.tags.get(id=3)
+        # Test specific match
+        tags = c.tags.get(raw_filters=[DBTag.title == 'test_daryl_2'])
         assert tags[0].title == 'test_daryl_2'
+        assert len(tags) == 1
+
+        # Test specific unmatch
+        tags = c.tags.get(raw_filters=[DBTag.title != 'test_daryl_2'])
+        assert tags[0].title == 'test_daryl_1'
+        assert len(tags) == 1
+
+        # Test bigger then
+        tags = c.tags.get(raw_filters=[DBTag.id > 0])
+        assert tags[0].title == 'test_daryl_1'
+        assert tags[1].title == 'test_daryl_2'
+        assert len(tags) == 2
+
+        # Test smaller then
+        tags = c.tags.get(raw_filters=[DBTag.id < 999])
+        assert tags[0].title == 'test_daryl_1'
+        assert tags[1].title == 'test_daryl_2'
+        assert len(tags) == 2
+
+
+def test_tags_named_filters(db: Database, normal_user: User) -> None:
+    """Test to retrieve tags for a specific user with named filters.
+
+    Should retrieve tags for the user in the context based on specific named
+    filters.
+
+    Args:
+        db: the database connection. Not used right now, but still in there to
+            make sure the database is connected.
+        normal_user: the model for the normal user.
+    """
+    with Context(user=normal_user) as c:
+        # Test specific match
+        tags = c.tags.get(title='test_daryl_2')
+        assert tags[0].title == 'test_daryl_2'
+        assert len(tags) == 1
+
+        # Test if something that is invalid is not found indeed
+        tags = c.tags.get(title='not_a_real_tag')
+        assert len(tags) == 0
+
+        # Test if a invalid field results in a exception
+        with raises(AttributeError):
+            tags = c.tags.get(unknown_field='test')
