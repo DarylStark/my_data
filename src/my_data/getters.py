dff4b4dff3eb38e26ad4fd0aac7e5734da4c447a
Daryl Stark <daryl@dstark.nl>
2023-06-30 14:09:30 +0200

    Updated documentation

--- src/my_data/getters.py
+++ src/my_data/getters.py
@@ -15,13 +15,20 @@ from .exceptions import PermissionDeniedException
 class Getter(CRUDBase):
     """Base Getter class.
 
-    TODO: Fill in
+    This class should be used when creating Getters. This base class defines
+    the basic metohds for getters: `get_default_filters` and `get`. It is based
+    from `CRUDBase` so we get the initializer and `is_authorized` methods.
     """
 
     def get_default_filters(self) -> list[ColumnElement]:
         """Default filters for the object.
 
-        TODO: Fill in
+        Method that returns the default filters for this specific getters. This
+        should be used when retrieving resources so only the correct resources
+        are retrieved. This method should be overridden by subclasses.
+
+        Returns:
+            A list with SQLModel filters.
         """
         return []
 
@@ -31,9 +38,18 @@ class Getter(CRUDBase):
 
         Gets the resources from the database and returns them. The client can
         give additional SQLmodel filters to filter the resources. By default,
-        the filters from the subclass get used.
+        only the filters from the subclass get used.
 
-        # TODO: Fill in
+        Args:
+            flt: a list with SQLModel filters to filter the data.
+
+        Returns:
+            A list with the retrieved objects or a empty list if not objects
+            were found.
+
+        Raises:
+            PermissionDeniedException: raisen when the user tries to retrieve
+                objects that he isn't allowed to retrieve.
         """
 
         # Check if the user is allowed to retrieve these resources
@@ -63,7 +79,7 @@ class Getter(CRUDBase):
 class UserSpecificGetter(Getter):
     """Getter for user specific resources.
 
-    TODO: Fill in
+    Retrieves resources that are user scoped, like Tags and APITokens.
     """
 
     def is_authorized(self) -> bool:
@@ -83,7 +99,10 @@ class UserSpecificGetter(Getter):
     def get_default_filters(self) -> list[ColumnElement]:
         """Give the default filters for this resource.
 
-        TODO: fill in
+        Returns the default filters for user scoped objects.
+
+        Returns:
+            A list with the default SQLModel filters for user scoped objects.
         """
         return [self._db_model.user_id == self._context_data.user.id]
 
@@ -91,17 +110,34 @@ class UserSpecificGetter(Getter):
 class UserGetter(Getter):
     """Getter for user resources.
 
-    TODO: Fill in
+    Retrieves resources that are users. Users can only retrieve their own user
+    account, except when they are a ROOT user; in that case, they can retrieve
+    all users.
     """
 
     def is_authorized(self) -> bool:
+        """Check if the current context can retrieve these objects.
+
+        Checks wheter the current context is allowed to retrieve these
+        specific resources. It only checks if the user is not None and if the
+        given model is a User.
+
+        Returns:
+            bool: True if the user is allowed to retrieve resources from this
+                type. False if the user is not allowed to do this.
+        """
         return (self._context_data.user is not None and
                 self._db_model is User)
 
     def get_default_filters(self) -> list[ColumnElement]:
         """Give the default filters for this resource.
 
-        TODO: fill in
+        Returns the default filters for users. For normal users, it returns a
+        filter that filters on the specific user. For root users, it doesn't
+        filter at all.
+
+        Returns:
+            A list with the default SQLModel filters for user scoped objects.
         """
         if self._context_data.user.role == UserRole.USER:
             return [User.id == self._context_data.user.id]
