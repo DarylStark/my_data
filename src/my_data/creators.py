a8b35f4d640159c3d9027953f89ddbe3e0cfe6e2
Daryl Stark <daryl@dstark.nl>
2023-07-14 11:33:05 +0200

    Created DOCSTRINGs for the Creators

--- src/my_data/creators.py
+++ src/my_data/creators.py
@@ -1,20 +1,44 @@
-"""TODO: documentation."""
+"""Module with Creators.
 
+This module contains the creator classes. These classes are used to create data
+in the database. The ResourceManager uses these classes.
+"""
+from typing import TypeVar
 from typing import Type
 
 from .data_manipulator import DataManipulator
 
+T = TypeVar('T')
+
 
 class Creator(DataManipulator):
-    """TODO: documentation."""
+    """Baseclass for Creators.
 
-    def create(self, models: list | Type) -> list:
-        pass
+    The baseclass for creators. The sub creators use this class to make sure
+    creators have the same interface."""
+
+    def create(self, models: list[T] | T) -> list[T]:
+        """Create data.
+
+        The method to create data in the database.
+
+        Args:
+            models: the models to create
+
+        Returns:
+            A list with the created data models.
+        """
 
 
 class UserScopedCreator(Creator):
-    """TODO: documentation."""
+    """Creator for UserScoped models.
+
+    This creator should be used for UserScoped models, like Tags and APItokens.
+    """
 
 
 class UserCreator(Creator):
-    """TODO: documentation."""
+    """Creator for Users.
+
+    This creator should be used to create Users.
+    """
