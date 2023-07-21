bdc31adedf410f9fa85db170d61bfd86d8596575
Daryl Stark <daryl@dstark.nl>
2023-07-21 12:38:08 +0200

    Created code to work with WebUISettings

--- src/my_data/context.py
+++ src/my_data/context.py
@@ -5,7 +5,7 @@ This module contains the Context class.
 from types import TracebackType
 
 from my_model.user_scoped_models import (APIClient, APIToken,  # type: ignore
-                                         Tag, User)
+                                         Tag, User, WebUISetting)
 from sqlalchemy.future import Engine
 
 from my_data.creators import UserCreator, UserScopedCreator
@@ -75,6 +75,14 @@ class Context:
             deleter=UserScopedDeleter)
         self.api_tokens = ResourceManager(
             database_model=APIToken,
+            database_engine=database_engine,
+            context_data=self._context_data,
+            creator=UserScopedCreator,
+            retriever=UserScopedRetriever,
+            updater=UserScopedUpdater,
+            deleter=UserScopedDeleter)
+        self.web_ui_settings = ResourceManager(
+            database_model=WebUISetting,
             database_engine=database_engine,
             context_data=self._context_data,
             creator=UserScopedCreator,
