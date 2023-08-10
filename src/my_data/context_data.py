01a54670de23ca2ff09a93e1bcf45e0521fa2e86
Daryl Stark <daryl@dstark.nl>
2023-08-10 21:44:26 +0200

    Added a `Session` to the ContextData.
    Fixes #44

--- src/my_data/context_data.py
+++ src/my_data/context_data.py
@@ -4,10 +4,10 @@ This module contains the `ContextData` class that can be used to specify the
 context-variables in which a `Context` object should operate.
 """
 from my_model.user_scoped_models import User  # type: ignore
-from pydantic.dataclasses import dataclass
+from sqlalchemy.future import Engine
+from sqlmodel import Session
 
 
-@dataclass
 class ContextData:
     """Class with ContextData.
 
@@ -16,6 +16,18 @@ class ContextData:
 
     Attributes:
         user: a user object representing this context.
+        db_session: a SQLalchemy session that can be used.
     """
 
-    user: User
+    def __init__(self, database_engine: Engine, user: User) -> None:
+        """Create the ContextData object.
+
+        The initiator sets the values for the ContextData and creates a
+        SQLalchmey Session.
+
+        Args:
+            database_engine: a SQLalchemy Engine to bind the Session to.
+            user: a User to bind the Context to
+        """
+        self.user: User = user
+        self.db_session = Session(database_engine, expire_on_commit=False)
