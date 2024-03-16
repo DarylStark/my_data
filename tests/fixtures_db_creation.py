94e14aade4fa32424f333b983207aa9a2aedc86c
Daryl Stark <daryl@dstark.nl>
2024-03-16 20:02:50 +0000

    Make testing config environment variables
    Fixes #154

--- tests/fixtures_db_creation.py
+++ tests/fixtures_db_creation.py
@@ -9,7 +9,19 @@ import os
 from my_data import MyData
 from my_data.data_loader import DataLoader, JSONDataSource
 from my_data.my_data_table_creator import MyDataTableCreator
+from pydantic_settings import BaseSettings
 from pytest import fixture
+
+
+class ConfigurationForTests(BaseSettings):
+    """Settings for testing."""
+
+    db_string: str = 'sqlite:///:memory:'
+    database_args: dict[str, str | bool | int] = {'echo': True}
+    service_username: str = 'service.user'
+    service_password: str = 'service_password'
+    create_tables: bool = True
+    import_data: bool = True
 
 
 def test_filename() -> str:
@@ -30,27 +42,31 @@ def my_data() -> MyData:
     Returns:
         The created `MyData` instance.
     """
+    configuration = ConfigurationForTests()
+
     # Configure the database
     my_data = MyData()
     my_data.configure(
-        db_connection_str='sqlite:///:memory:',
-        database_args={'echo': True},
-        service_username='service.user',
-        service_password='service_password',
+        db_connection_str=configuration.db_string,
+        database_args=configuration.database_args,
+        service_username=configuration.service_username,
+        service_password=configuration.service_password,
     )
 
     # Create the engine
     my_data.create_engine()
 
     # Create the tables
-    my_data_table_creator = MyDataTableCreator(my_data_object=my_data)
-    my_data_table_creator.create_db_tables(drop_tables=True)
+    if configuration.create_tables:
+        my_data_table_creator = MyDataTableCreator(my_data_object=my_data)
+        my_data_table_creator.create_db_tables(drop_tables=True)
 
     # Create testdata
-    loader = DataLoader(
-        my_data_object=my_data, data_source=JSONDataSource(test_filename())
-    )
-    loader.load()
+    if configuration.import_data:
+        loader = DataLoader(
+            my_data_object=my_data, data_source=JSONDataSource(test_filename())
+        )
+        loader.load()
 
     # Return the created object
     return my_data
